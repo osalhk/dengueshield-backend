8c838a7128867c4b4add48ced2a608b40b6c2ae1
Osal Geesara <geesarahko.23@uom.lk>
2026-07-09 17:01:48 +0530

    feat: implement authMiddleware for Firebase ID token verification

---
+++ dengueshield-backend/src/middleware/authMiddleware.js
@@ -0,0 +1,44 @@
+const admin = require("../config/firebaseAdmin");
+
+const authMiddleware = async (req, res, next) => {
+  try {
+    // Get Authorization header
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      return res.status(401).json({
+        success: false,
+        message: "Authorization header missing",
+      });
+    }
+
+    // Expected format:
+    // Authorization: Bearer <token>
+
+    const token = authHeader.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Token missing",
+      });
+    }
+
+    // Verify Firebase ID Token
+    const decodedToken = await admin.auth().verifyIdToken(token);
+
+    // Attach authenticated user
+    req.user = decodedToken;
+
+    next();
+  } catch (error) {
+    console.error(error);
+
+    return res.status(401).json({
+      success: false,
+      message: "Invalid or expired token",
+    });
+  }
+};
+
+module.exports = authMiddleware;
